f940220db3400eb750bbf131d381581e0b68e261
Pedro Alvarado <pedro.alvarado@globant.com>
2015-11-18 14:31:19 -0500

    Improve search (filter and regexp)

--- frontend/app/controllers/products.js
+++ frontend/app/controllers/products.js
@@ -3,8 +3,12 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   application: Ember.inject.controller(),
   filteredContent: Ember.computed('model', 'application.search', function() {
-    var products = this.model;
-    products = products.filterBy('title', this.get('application.search'));
+    var products = this.model,
+        search = new RegExp(this.get('application.search'), 'i');
+    products = products.filter(function(product) {
+      var str = product.get('title') + product.get('description');
+      return search.test(str);
+    });
     return products;
   })
 });
