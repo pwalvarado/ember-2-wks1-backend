5a713892887181664898dc16fd9bd20c90171178
Pedro Alvarado <pedro.alvarado@globant.com>
2015-11-18 14:50:39 -0500

    More readable

--- frontend/app/controllers/products.js
+++ frontend/app/controllers/products.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   application: Ember.inject.controller(),
-  filteredContent: Ember.computed('model', 'application.search', 'model.@each.title', 'model.@each.description', function() {
+  search: Ember.computed.alias('application.search'),
+  filteredContent: Ember.computed('model', 'search', 'model.@each.title', 'model.@each.description', function() {
     var products = this.model,
-        search = new RegExp(this.get('application.search'), 'i');
+        search = new RegExp(this.get('search'), 'i');
+
     products = products.filter(function(product) {
       var str = product.get('title') + product.get('description');
       return search.test(str);
