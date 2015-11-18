9354be601e321c957f3b8b396283eddd7d1b28b3
Pedro Alvarado <pedro.alvarado@globant.com>
2015-11-18 14:40:51 -0500

    more computed properties

--- frontend/app/controllers/products.js
+++ frontend/app/controllers/products.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   application: Ember.inject.controller(),
-  filteredContent: Ember.computed('model', 'application.search', function() {
+  filteredContent: Ember.computed('model', 'application.search', 'model.@each.title', 'model.@each.description', function() {
     var products = this.model,
         search = new RegExp(this.get('application.search'), 'i');
     products = products.filter(function(product) {
