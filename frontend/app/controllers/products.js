03642b97b7f5064e3966675eb643d651a4e45167
Pedro Alvarado <pedro.alvarado@globant.com>
2015-11-18 13:58:02 -0500

    Simple filter approach

--- frontend/app/controllers/products.js
+++ frontend/app/controllers/products.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   application: Ember.inject.controller(),
-  filteredContent: Ember.computed('model', function() {
+  filteredContent: Ember.computed('model', 'application.search', function() {
     var products = this.model;
-
+    products = products.filterBy('title', this.get('application.search'));
     return products;
   })
 });
